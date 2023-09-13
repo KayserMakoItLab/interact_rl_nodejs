d384afa5713130dc5f953ac9f0aea19b2fb8aa32
karthiktaff <karthik@taffinc.com>
2023-09-13 13:58:53 +0530

    update response in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -9,6 +9,11 @@ const reportConsolidationController = async (req, res) => {
 
     const response = await getReportDetails(startDate, endDate, type);
 
+    res.send({
+      status: 200,
+      message: "Process Started!",
+    });
+
     if (response && type === "project") {
       data = await generateReportByProjectService(response.downloadUrl);
     } else if (response && type === "user") {
@@ -16,11 +21,6 @@ const reportConsolidationController = async (req, res) => {
     } else {
       res.send({ status: 400, message: 'something went wrong' });
     }
-
-    res.send({
-      status: 200,
-      message: "Process completed!",
-    });
   } catch (error) {
     console.log("error", error);
     res.send({ status: 400, message: error });
