f05e3139beea4f2c91832d86f0994016c9663b08
Kayser <kayser@makoitlab.com>
2023-11-06 13:23:40 +0530

    Updated category report generation

--- services/generateReportByCategoryService.js
+++ services/generateReportByCategoryService.js
@@ -13,11 +13,6 @@ const generateReportByCategoryService = async (url, id, email) => {
   try {
     const workbook = new ExcelJS.Workbook();
     const worksheet = workbook.addWorksheet("Sheet 1");
-    worksheet.addRow(headers);
-    worksheet.addRow(subHeaders);
-    worksheet.mergeCells("B1:L1");
-    worksheet.mergeCells("M1:V1");
-    worksheet.mergeCells("W1:Z1");
     console.log("url", url);
     await downloadFile(
       url,
@@ -40,16 +35,14 @@ const generateReportByCategoryService = async (url, id, email) => {
         });
     });
 
-    // console.log("generatedReport",generatedReport)
-
     const data = await Promise.all(
       generatedReport.map(async (result) => {
         if (
-          (result.Billable === "true" && (result.ProjectId !== "") ||
-          result.ProjectId !== null)
+          (((result.ProjectId !== "") ||
+          result.ProjectId !== null) && (result.Billable === "true"))
          ) {
           const projectInfo = await getProjectDetailsById(result.ProjectId);
-          console.log("projectInfo?.fields", projectInfo?.fields);
+          console.log("projectInfo?.fields", result.Billable);
           const categoryValue = await (projectInfo?.fields || []).find(
             (value) => value?.fieldLabel === "Category"
           )?.fieldValue;
@@ -59,129 +52,184 @@ const generateReportByCategoryService = async (url, id, email) => {
       })
     );
 
+    const filteredData = data.filter((item) => item !== null);
 
-    console.log("data", data);
 
-    const result = data.reduce((agg, each) => {
-      if (!agg[each.catergoryFieldName]) {
-        const { CategoryName } = each;
-        agg[each.catergoryFieldName] = {
-          userTrackedTime: 0,
-          totalTracked: 0,
-          project: {},
-        };
-      }
-      agg[each.catergoryFieldName].userTrackedTime =
-        agg[each.catergoryFieldName].userTrackedTime + +each.TrackedTime;
-      agg[each.catergoryFieldName].totalTracked =
-        agg[each.catergoryFieldName].totalTracked + 1;
+     const result = filteredData.reduce((agg, each) => {
+       if (!agg[each.catergoryFieldName]) {
+         agg[each.catergoryFieldName] = {
+           users: {},
+         };
+       }
+       if (!agg[each.catergoryFieldName].users[each.UserName]) {
+         agg[each.catergoryFieldName].users[each.UserName] = {
+           trackedTimes: 0,
+         };
+       }
+       agg[each.catergoryFieldName].users[each.UserName].trackedTimes =
+         agg[each.catergoryFieldName].users[each.UserName].trackedTimes +
+         +each.TrackedTime;
 
-      if (!agg[each.catergoryFieldName].project[each.ProjectId]) {
-        agg[each.catergoryFieldName].project[each.ProjectId] = {
-          total: 0,
-          entries: 0,
-          task: {},
-        };
-      }
+       return agg;
+     }, {});
 
-      agg[each.catergoryFieldName].project[each.ProjectId].total =
-        agg[each.catergoryFieldName].project[each.ProjectId].total +
-        +each.TrackedTime;
+     let headerNames = []
+     for (const [value, item] of Object.entries(result)) {
+      if (value !== "undefined") {
+        for (const [user, time] of Object.entries(item.users)) {
+          !headerNames.includes(user) && headerNames.push(user);
+        }
+      } 
+    }
 
-      if (
-        !agg[each.catergoryFieldName].project[each.ProjectId].task[each.TaskId]
-      ) {
-        agg[each.catergoryFieldName].project[each.ProjectId].task[each.TaskId] =
-          {
-            total: 0,
-            data: [],
-          };
-      }
+    headerNames.unshift('Group By');
+    worksheet.addRow(headerNames);
 
-      agg[each.catergoryFieldName].project[each.ProjectId].task[
-        each.TaskId
-      ].total =
-        agg[each.catergoryFieldName].project[each.ProjectId].task[each.TaskId]
-          .total + +each.TrackedTime;
-
-      agg[each.catergoryFieldName].project[each.ProjectId].task[
-        each.TaskId
-      ].data.push(each);
-
-      return agg;
-    }, {});
-
-    const i = 0;
-    for (const [category, item] of Object.entries(result)) {
-      for (const [projectId, value] of Object.entries(item.project)) {
-        const projectInfo = await getProjectDetailsById(projectId);
-        for (const [taskId, entries] of Object.entries(value.task)) {
-          const taskInfo = await getTaskDetailsById(taskId);
-          for (const entry of entries.data) {
-            if (
-              entry?.ProjectId !== "" &&
-              entry?.Billable === "true" &&
-              entry?.catergoryFieldName
-              ) {
-              console.log("entry?.catergoryFieldName", entry?.catergoryFieldName);
-              if (i % 100 == 0) {
-                await new Promise((res, rej) => setTimeout(() => res(), 1000));
-              }
-
-              const completedDate = moment(
-                new Date(taskInfo?.completedAt)
-              ).format("YYYY-MM-DD");
-
-              const managerName = projectInfo?.createdBy?.firstName
-                ? projectInfo?.createdBy?.firstName +
-                  " " +
-                  projectInfo?.createdBy?.lastName
-                : "";
-
-              const categoryValue = (projectInfo?.fields || []).find(
-                (value) => value?.fieldLabel === "Category"
-              )?.fieldValue;
-
-              const reportRow = {
-                groupBy: category,
-                number: projectId,
-                title: projectInfo?.projectName,
-                category: categoryValue,
-                projectClient: projectInfo?.customer?.companyName,
-                customStatus: projectInfo?.status,
-                manager: managerName,
-                projectStart: projectInfo?.startDate,
-                projectDue: projectInfo?.dueDate,
-                taskTimeAllocated: projectInfo?.metrics?.totalAllocatedHours,
-                projectTotalTimeSpent: projectInfo?.metrics?.trackedHours,
-                projectFilteredTimeSpent: value?.totalTracked,
-                order: taskInfo?.taskId,
-                taskName: taskInfo?.taskName,
-                contacts: taskInfo?.assignee?.users[0]?.userName,
-                status: taskInfo?.status,
-                taskStart: taskInfo?.startDate,
-                taskDue: taskInfo?.dueDate,
-                completed:
-                  completedDate === "Invalid date" ? "" : completedDate,
-                timeAllocated: entry.Effort,
-                taskTotalTimeSpent:
-                  taskInfo?.effort && taskInfo?.effort > 0
-                    ? taskInfo?.effort / 60
-                    : 0,
-                taskFilteredTimeSpent: entries.total,
-                timeRecords: entry.Notes,
-                timer: entry.Date,
-                staff: entry.UserName,
-                timeSpent: entry.TrackedTime,
-              };
-
-              console.log("reportRow", reportRow);
-              worksheet.addRow(Object.values(reportRow));
-            }
-          }
+    let i = 1
+    for (const [value, item] of Object.entries(result)) {
+      if (value !== "undefined") {
+        for (const [user, time] of Object.entries(item.users)) {
+          worksheet.eachRow({ includeEmpty: false }, (row, rowNumber) => {
+              row.eachCell((cell, colNumber) => {
+                const cellName = getCellName(rowNumber, colNumber);
+                if (cell.value === user) {
+                  cellName;
+                  const insertValue = cellName.replace(1, i);
+                  const getPrevValue = worksheet.getCell(insertValue);
+                  getPrevValue.value = time.trackedTimes;
+                } else if(cell.value === 'Group By'){
+                  const insertValue = cellName.replace(1, i);
+                  const getPrevValue = worksheet.getCell(insertValue);
+                  getPrevValue.value = value;
+                }
+              });
+          });
         }
       }
+      i++;
     }
+
+
+    // const result = data.reduce((agg, each) => {
+    //   if (!agg[each.catergoryFieldName]) {
+    //     const { CategoryName } = each;
+    //     agg[each.catergoryFieldName] = {
+    //       userTrackedTime: 0,
+    //       totalTracked: 0,
+    //       project: {},
+    //     };
+    //   }
+    //   agg[each.catergoryFieldName].userTrackedTime =
+    //     agg[each.catergoryFieldName].userTrackedTime + +each.TrackedTime;
+    //   agg[each.catergoryFieldName].totalTracked =
+    //     agg[each.catergoryFieldName].totalTracked + 1;
+
+    //   if (!agg[each.catergoryFieldName].project[each.ProjectId]) {
+    //     agg[each.catergoryFieldName].project[each.ProjectId] = {
+    //       total: 0,
+    //       entries: 0,
+    //       task: {},
+    //     };
+    //   }
+
+    //   agg[each.catergoryFieldName].project[each.ProjectId].total =
+    //     agg[each.catergoryFieldName].project[each.ProjectId].total +
+    //     +each.TrackedTime;
+
+    //   if (
+    //     !agg[each.catergoryFieldName].project[each.ProjectId].task[each.TaskId]
+    //   ) {
+    //     agg[each.catergoryFieldName].project[each.ProjectId].task[each.TaskId] =
+    //       {
+    //         total: 0,
+    //         data: [],
+    //       };
+    //   }
+
+    //   agg[each.catergoryFieldName].project[each.ProjectId].task[
+    //     each.TaskId
+    //   ].total =
+    //     agg[each.catergoryFieldName].project[each.ProjectId].task[each.TaskId]
+    //       .total + +each.TrackedTime;
+
+    //   agg[each.catergoryFieldName].project[each.ProjectId].task[
+    //     each.TaskId
+    //   ].data.push(each);
+
+    //   return agg;
+    // }, {});
+
+    // const i = 0;
+    // for (const [category, item] of Object.entries(result)) {
+    //   for (const [projectId, value] of Object.entries(item.project)) {
+    //     const projectInfo = await getProjectDetailsById(projectId);
+    //     for (const [taskId, entries] of Object.entries(value.task)) {
+    //       const taskInfo = await getTaskDetailsById(taskId);
+    //       for (const entry of entries.data) {
+    //         if (
+    //           entry?.ProjectId !== "" &&
+    //           entry?.Billable === "true" &&
+    //           entry?.catergoryFieldName
+    //           ) {
+    //           console.log("entry?.catergoryFieldName", entry?.catergoryFieldName);
+    //           if (i % 100 == 0) {
+    //             await new Promise((res, rej) => setTimeout(() => res(), 1000));
+    //           }
+
+    //           const completedDate = moment(
+    //             new Date(taskInfo?.completedAt)
+    //           ).format("YYYY-MM-DD");
+
+    //           const managerName = projectInfo?.createdBy?.firstName
+    //             ? projectInfo?.createdBy?.firstName +
+    //               " " +
+    //               projectInfo?.createdBy?.lastName
+    //             : "";
+
+    //           const categoryValue = (projectInfo?.fields || []).find(
+    //             (value) => value?.fieldLabel === "Category"
+    //           )?.fieldValue;
+
+              // const reportRow = {
+              //   groupBy: category,
+              //   number: projectId,
+              //   title: projectInfo?.projectName,
+              //   category: categoryValue,
+              //   projectClient: projectInfo?.customer?.companyName,
+              //   customStatus: projectInfo?.status,
+              //   manager: managerName,
+              //   projectStart: projectInfo?.startDate,
+              //   projectDue: projectInfo?.dueDate,
+              //   taskTimeAllocated: projectInfo?.metrics?.totalAllocatedHours,
+              //   projectTotalTimeSpent: projectInfo?.metrics?.trackedHours,
+              //   projectFilteredTimeSpent: value?.totalTracked,
+              //   order: taskInfo?.taskId,
+              //   taskName: taskInfo?.taskName,
+              //   contacts: taskInfo?.assignee?.users[0]?.userName,
+              //   status: taskInfo?.status,
+              //   taskStart: taskInfo?.startDate,
+              //   taskDue: taskInfo?.dueDate,
+              //   completed:
+              //     completedDate === "Invalid date" ? "" : completedDate,
+              //   timeAllocated: entry.Effort,
+              //   taskTotalTimeSpent:
+              //     taskInfo?.effort && taskInfo?.effort > 0
+              //       ? taskInfo?.effort / 60
+              //       : 0,
+              //   taskFilteredTimeSpent: entries.total,
+              //   timeRecords: entry.Notes,
+              //   timer: entry.Date,
+              //   staff: entry.UserName,
+              //   timeSpent: entry.TrackedTime,
+              // };
+
+              // console.log("reportRow", reportRow);
+              // worksheet.addRow(Object.values(reportRow));
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
 
     await deleteReportDetails(id);
     worksheet.getRow(1).alignment = { horizontal: "center" };
@@ -200,6 +248,11 @@ const generateReportByCategoryService = async (url, id, email) => {
     return error;
   }
 };
+
+function getCellName(rowNumber, colNumber) {
+  const columnName = String.fromCharCode(64 + colNumber);
+  return columnName + rowNumber;
+}
 
 module.exports = {
   generateReportByCategoryService,
